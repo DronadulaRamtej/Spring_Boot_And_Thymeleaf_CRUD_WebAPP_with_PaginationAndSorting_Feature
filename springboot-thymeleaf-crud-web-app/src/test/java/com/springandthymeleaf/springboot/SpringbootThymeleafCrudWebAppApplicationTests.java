6d96c4c799c343bb72314899c9169ec1a991499f
DronadulaRamtej <nani.ramtej@gmail.com>
2024-02-28 20:38:05 -0500

    Created SpringBoot app and added templates

---
+++ springboot-thymeleaf-crud-web-app/src/test/java/com/springandthymeleaf/springboot/SpringbootThymeleafCrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springandthymeleaf.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafCrudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
